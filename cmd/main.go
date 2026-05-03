055273ae7bfa903068f39d46c4e25455a3eda1e6
prostraction <secret.darklight@gmail.com>
2026-05-04 00:30:38 +0500

    feat: bump ver

--- cmd/main.go
+++ cmd/main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/nanorele/gio/app"
 )
 
-const appTitle = "Rete 0.4.3"
+const appTitle = "Rete 0.4.4"
 const bugReportURL = "https://github.com/nanorele/rete/issues/new"
 
 func main() {
